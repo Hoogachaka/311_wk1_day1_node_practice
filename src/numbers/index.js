2898a458a0206b0abbf6a8852708da72ee800ee9
Hoogachaka <thatdylan@gmail.com>
2022-10-26 20:20:49 -0500

    are we there yet

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,25 @@
 const isEven = (num) => {
-  // write code for numbers.isEven
-
+  if (num % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const sum = (arr) => {
-  // write code for numbers.sum
-
+  let arrSum = arr.reduce(
+    (a, b) => a + b);
+    return arrSum
 }
 
 const comboSum = (arr, sum) => {
-  // write code for numbers.comboSum
-
+  let arrSum = arr.reduce(
+    (a, b) => a + b);
+  if (arrSum == sum) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = {
