c5206733ff99c0dc54417fb4f5b7b89429867dad
Hoogachaka <thatdylan@gmail.com>
2022-10-26 20:38:16 -0500

    added bad code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const numbers = require('./numbers')
 const strings = require('./strings')
 const dates = require('./dates')
 
+const split = (string, delimiter) => {
+  return string.split(`${delimiter}`)
+}
+
 module.exports = {
   numbers,
   strings,
